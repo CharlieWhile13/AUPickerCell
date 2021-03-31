2832a424ae8c583f18ffee68c57896bdcef334a4
Amy While <26681721+CharlieWhile13@users.noreply.github.com>
2021-03-31 19:46:59 +0100

    oh I didn't commit it

--- Sources/AUPickerCell/AUPickerCell.swift
+++
@@ -1,3 +0,0 @@
-struct AUPickerCell {
-    var text = "Hello, World!"
-}
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import AUPickerCellTests
-
-var tests = [XCTestCaseEntry]()
-tests += AUPickerCellTests.allTests()
-XCTMain(tests)
